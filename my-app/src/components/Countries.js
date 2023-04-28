29e5225a7166bd64fc1db58b0ee0175ba93576e5
tanujaru <tanujaru@gmail.com>
2023-04-28 17:32:33 -0400

    commit4

--- my-app/src/components/Countries.js
+++ my-app/src/components/Countries.js
@@ -1,110 +1,133 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Filter from "./Filter"
+import { useState, useEffect } from "react";
+import Article from "./Article";
 
-const url = "https://restcountries.eu/rest/v2/all"
-
-const Countries = () => {
-  const [countries, setCountries] = useState([])
-  const [filtered, setFiltered] = useState([])
-  const [searchInput, setSearchInput] = useState("")
-  const [isLoading, setIsLoading] = useState(true)
+export default function Countries() {
+  const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const regions = [
+    {
+      name: "Europe",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "Oceania",
+    },
+    {
+      name: "Americas",
+    },
+    {
+      name: "Antarctic",
+    },
+  ];
 
   useEffect(() => {
-    const fetchCountries = async () => {
-      const response = await fetch(url)
-      const countries = await response.json()
-      setCountries(countries)
-      setIsLoading(false)
+    document.title = `Showing All Countries`;
+  }, []);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const data = await res.json();
+        setCountries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCountries();
+  }, []);
+
+  async function searchCountry() {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${searchText}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    fetchCountries()
-  }, [])
+  async function filterByRegion(region) {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/region/${region}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  // const removeCountry = (numericCode) => {
-  //   const newCountry = countries.filter(
-  //     (country) => country.numericCode !== numericCode
-  //   )
-  //   setCountries(newCountry)
-  // }
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
+  }
+
+  function handleFilterByRegion(e) {
+    e.preventDefault();
+    filterByRegion();
+  }
 
   return (
     <>
-      <Filter
-        searchInput={searchInput}
-        setSearchInput={setSearchInput}
-        setFiltered={setFiltered}
-        setCountries={setCountries}
-        countries={countries}
-      />
-      {isLoading ? (
-        <h1 className="loading">Loading...</h1>
-      ) : searchInput.length > 1 ? (
-        <section className="countries">
-          {filtered.map((country) => {
-            const { numericCode, name, flag, population, region, capital } =
-              country
-
-            return (
-              <Link to={`/countries/${name}`} key={numericCode}>
-                <article>
-                  <div className="flag">
-                    <img src={flag} alt={name} />
-                  </div>
-                  <div className="details">
-                    <h4 className="country-name">
-                      Name: <span>{name}</span>
-                    </h4>
-                    <h4>
-                      Population: <span>{population.toLocaleString()}</span>
-                    </h4>
-                    <h4>
-                      Region: <span>{region}</span>
-                    </h4>
-                    <h4>
-                      Capital: <span>{capital}</span>
-                    </h4>
-                  </div>
-                </article>
-              </Link>
-            )
-          })}
-        </section>
+      {!countries ? (
+        <h1 className="text-gray-900 font-bold uppercase tracking-wide flex items-center justify-center text-center h-screen text-4xl dark:text-white">
+          Loading...
+        </h1>
       ) : (
-        <section className="countries">
-          {countries.map((country) => {
-            const { numericCode, name, flag, population, region, capital } =
-              country
+        <section className="container mx-auto p-8">
+          {/* form */}
+          <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
+            <form
+              onSubmit={handleSearchCountry}
+              autoComplete="off"
+              className="max-w-4xl md:flex-1"
+            >
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search for a country by its name"
+                required
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow rounded outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"
+              />
+            </form>
 
-            return (
-              <Link to={`/countries/${name}`} key={numericCode}>
-                <article>
-                  <div className="flag">
-                    <img src={flag} alt={name} />
-                  </div>
-                  <div className="details">
-                    <h4 className="country-name">
-                      Name: <span>{name}</span>
-                    </h4>
-                    <h4>
-                      Population: <span>{population.toLocaleString()}</span>
-                    </h4>
-                    <h4>
-                      Region: <span>{region}</span>
-                    </h4>
-                    <h4>
-                      Capital: <span>{capital}</span>
-                    </h4>
-                  </div>
-                </article>
-              </Link>
-            )
-          })}
+            <form onSubmit={handleFilterByRegion}>
+              <select
+                name="filter-by-region"
+                id="filter-by-region"
+                className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700"
+                value={regions.name}
+                onChange={(e) => filterByRegion(e.target.value)}
+              >
+                {regions.map((region, index) => (
+                  <option key={index} value={region.name}>
+                    {region.name}
+                  </option>
+                ))}
+              </select>
+            </form>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {countries.map((country) => (
+              <Article key={country.name.common} {...country} />
+            ))}
+          </div>
         </section>
       )}
     </>
-  )
+  );
 }
-
-export default Countries
-
--- my-app/src/components/Country.js
+++ my-app/src/components/Country.js
@@ -1,104 +1,80 @@
-import React, { useState, useEffect } from "react"
-import { Link, useParams } from "react-router-dom"
-import "../country.css"
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const Country = () => {
-  const [country, setCountry] = useState([])
-  const { name } = useParams()
+export default function SingleCountry() {
+  const [country, setCountry] = useState([]);
+  const { name } = useParams();
 
   useEffect(() => {
-    const fetchCountryData = async () => {
-      const response = await fetch(
-        `https://restcountries.eu/rest/v2/name/${name}`
-      )
-      const country = await response.json()
-      setCountry(country)
-    }
+    const getSingleCountry = async () => {
+      try {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+        const data = await res.json();
+        setCountry(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    fetchCountryData()
-  }, [name])
+    getSingleCountry();
+  }, [name]);
+
+  useEffect(() => {
+    document.title = `Countries | ${name}`;
+  }, [name]);
 
   return (
     <>
-      <section className="country">
-        <Link to="/" className="btn btn-light">
-          <i className="fas fa-arrow-left"></i> Back Home
-        </Link>
-        {country.map((c) => {
-          const {
-            numericCode,
-            flag,
-            name,
-            nativeName,
-            population,
-            region,
-            subregion,
-            capital,
-            topLevelDomain,
-            currencies,
-            languages,
-            borders,
-          } = c
-
-          return (
-            <article key={numericCode}>
-              <div className="country-inner">
-                <div className="flag">
-                  <img src={flag} alt={name} />
-                </div>
-
-                <div className="country-details">
-                  <div>
-                    <h2>{name}</h2>
-                    <h5>
-                      Native Name: <span>{nativeName}</span>
-                    </h5>
-                    <h5>
-                      Population: <span>{population.toLocaleString()}</span>
-                    </h5>
-                    <h5>
-                      Region: <span>{region}</span>
-                    </h5>
-                    <h5>
-                      Sub Region: <span>{subregion}</span>
-                    </h5>
-                    <h5>
-                      Capital: <span>{capital}</span>{" "}
-                    </h5>
-                  </div>
-
-                  <div>
-                    <h5>
-                      Top Level Domain: <span>{topLevelDomain}</span>
-                    </h5>
-                    <h5>
-                      Currencies: <span>{currencies[0].name}</span>
-                    </h5>
-                    <h5>
-                      Languages: <span>{languages[0].name}</span>
-                    </h5>
-                  </div>
-                </div>
-              </div>
-
-              <div>
-                <h3>Border Countries: </h3>
-                <div className="borders">
-                  {borders.map((border) => {
-                    return (
-                      <ul key={border}>
-                        <li>{border}</li>
-                      </ul>
-                    )
-                  })}
-                </div>
-              </div>
+      <section className="p-8 md:py-0 max-w-7xl mx-auto">
+        {country.map((item) => (
+          <div
+            key={item.population}
+            className="grid grid-cols-1 gap-8 md:grid-cols-2 md:place-items-center md:h-screen"
+          >
+            <article>
+              <img src={item.flags.svg} alt={item.name.common} />
             </article>
-          )
-        })}
+
+            <article>
+              <h1 className="mb-8 font-bold text-gray-900 dark:text-white text-4xl lg:text-6xl">
+                {item.name.official}
+              </h1>
+
+              <ul className="my-4 flex flex-col items-start justify-start gap-2 text-slate-700 dark:text-gray-400">
+                <li>Capital: {item.capital[0]}</li>
+                <li>Population: {item.population.toLocaleString()}</li>
+                <li>Region: {item.region}</li>
+                <li>Subregion: {item.subregion}</li>
+              </ul>
+
+              {item.borders && (
+                <>
+                  <h3 className="text-gray-900 font-bold text-lg mb-2 dark:text-white">
+                    Borders:
+                  </h3>
+                  <ul className="flex flex-wrap items-start justify-start gap-2">
+                    {item.borders.map((border, index) => (
+                      <li
+                        key={index}
+                        className="bg-white p-2 rounded text-xs tracking-wide shadow dark:bg-gray-800 dark:text-gray-400 text-gray-700"
+                      >
+                        {border}
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+
+              <Link
+                to="/"
+                className="inline-block mt-8 bg-white py-2 px-6 rounded shadow text-gray-700 hover:bg-gray-200 transition-all duration-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-400"
+              >
+                &larr; Back
+              </Link>
+            </article>
+          </div>
+        ))}
       </section>
     </>
-  )
+  );
 }
-
-export default Country
